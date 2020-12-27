da0b70a55530672b69d8d63df81f2646c0dac2ac
alejandrocortell <alejandrocortellmarin@gmail.com>
2020-12-27 11:43:04 +0100

    Componente RestautantCard maquetado

---
+++ components/RestaurantCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card" style="width:400px;">
+    <div class="card-image">
+      <figure class="image">
+        <img src="~/assets/images/restaurant.jpg" alt="Pizza Restaurant" />
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4 restaurant-name">
+            {{ name }}
+          </p>
+          <div class="columns">
+            <div class="column">
+              <span class="tag">{{ category }}</span>
+            </div>
+            <div class="column has-text-right">
+              <button 
+                class="button is-info"
+                v-on:click="sumLikes"
+                >
+                {{ likes }}
+                </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        {{ description }}
+        <a v-bind:href="slug">more info</a>.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Restaurant Name',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus nec iaculis mauris',
+      likes: 0,
+      category: 'burger',
+      slug: 'restaurant-name'
+    }
+  },
+  methods: {
+    sumLikes() {
+      this.likes++
+    }
+  }
+}
+</script>
+
+<style scoped>
+.restaurant-name {
+  font-size: 30px;
+  color: #8ae6ff;
+}
+</style>
